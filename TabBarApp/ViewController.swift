da18c57f7ef57f7c63cdbb20a8ef8d585acdb0e7
szholbarys <sakenzholbarys@gmail.com>
2024-11-21 22:08:47 +0500

    Initial Commit

---
+++ TabBarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarApp
+//
+//  Created by Zholbarys on 21.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
